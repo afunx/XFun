57cb25fb41f04ede1b091c7246cf51b456ee7444
afunx <afunx.bai@ubtrobot.com>
2020-09-03 21:41:30 +0800

    LogUtils.java添加线程名打印

--- app/src/main/java/com/ubtechinc/aimbothumming/utils/LogUtils.java
+++ app/src/main/java/com/ubtechinc/aimbothumming/utils/LogUtils.java
@@ -18,51 +18,51 @@ public class LogUtils {
 
     public static void v(String tag, String message) {
         if (sDebug) {
-            Log.v(TAG, tag + "::" + message);
+            Log.v(TAG, tag + "::" + Thread.currentThread().getName() + ": " + message);
         }
     }
 
     public static void vv(String tag, String message) {
-        Log.v(TAG, tag + "::" + message);
+        Log.v(TAG, tag + "::" + Thread.currentThread().getName() + ": " + message);
     }
 
     public static void d(String tag, String message) {
         if (sDebug) {
-            Log.d(TAG, tag + "::" + message);
+            Log.d(TAG, tag + "::" + Thread.currentThread().getName() + ": " + message);
         }
     }
 
     public static void dd(String tag, String message) {
-        Log.d(TAG, tag + "::" + message);
+        Log.d(TAG, tag + "::" + Thread.currentThread().getName() + ": " + message);
     }
 
     public static void i(String tag, String message) {
         if (sDebug) {
-            Log.i(TAG, tag + "::" + message);
+            Log.i(TAG, tag + "::" + Thread.currentThread().getName() + ": " + message);
         }
     }
 
     public static void ii(String tag, String message) {
-        Log.i(TAG, tag + "::" + message);
+        Log.i(TAG, tag + "::" + Thread.currentThread().getName() + ": " + message);
     }
 
     public static void w(String tag, String message) {
         if (sDebug) {
-            Log.w(TAG, tag + "::" + message);
+            Log.w(TAG, tag + "::" + Thread.currentThread().getName() + ": " + message);
         }
     }
 
     public static void ww(String tag, String message) {
-        Log.w(TAG, tag + "::" + message);
+        Log.w(TAG, tag + "::" + Thread.currentThread().getName() + ": " + message);
     }
 
     public static void e(String tag, String message) {
         if (sDebug) {
-            Log.e(TAG, tag + "::" + message);
+            Log.e(TAG, tag + "::" + Thread.currentThread().getName() + ": " + message);
         }
     }
 
     public static void ee(String tag, String message) {
-        Log.e(TAG, tag + "::" + message);
+        Log.e(TAG, tag + "::" + Thread.currentThread().getName() + ": " + message);
     }
 }
