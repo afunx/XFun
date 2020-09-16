bce7a8bff5cf5382e163b09d6f8a01a1ca87aa6a
afunx <afunx.bai@ubtrobot.com>
2020-09-16 21:19:58 +0800

    add Utils.java and put getViewHeight(View) method

---
+++ common/src/main/java/com/afunx/xfun/common/utils/Utils.java
@@ -0,0 +1,24 @@
+package com.afunx.xfun.common.utils;
+
+import android.view.View;
+
+import java.lang.reflect.Method;
+
+public class Utils {
+
+    public static int getViewHeight(View v) {
+        try {
+            Method m = v.getClass().getDeclaredMethod("onMeasure", int.class,
+                    int.class);
+            m.setAccessible(true);
+            m.invoke(v, View.MeasureSpec.makeMeasureSpec(
+                    ((View) v.getParent()).getMeasuredWidth(),
+                    View.MeasureSpec.AT_MOST), View.MeasureSpec.makeMeasureSpec(0,
+                    View.MeasureSpec.UNSPECIFIED));
+        } catch (Exception e) {
+
+        }
+        return v.getMeasuredHeight();
+    }
+
+}
