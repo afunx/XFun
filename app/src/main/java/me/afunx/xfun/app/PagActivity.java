1df08f84741a48abc523dcea6f7f3e160c792b59
afunx <afunx.bai@ubtrobot.com>
2023-05-07 16:34:21 +0800

    支持帧率显示以及Demo合成效果

--- app/src/main/java/me/afunx/xfun/app/PagActivity.java
+++ app/src/main/java/me/afunx/xfun/app/PagActivity.java
@@ -4,16 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.databinding.ViewDataBinding;
 
+import android.graphics.Matrix;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.SystemClock;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.afunx.xfun.common.utils.LogUtils;
 
+import org.libpag.PAGComposition;
+import org.libpag.PAGDiskCache;
 import org.libpag.PAGFile;
 import org.libpag.PAGImageView;
+import org.libpag.PAGPlayer;
+import org.libpag.PAGTimeStretchMode;
 import org.libpag.PAGView;
 
 import me.afunx.xfun.app.util.TimeDiffUtil;
@@ -24,34 +33,190 @@ public class PagActivity extends AppCompatActivity {
 
     private PAGImageView mPAGView;
 
+    private TextView mTvFrameRate;
+
+    private Handler mMainHandler = new Handler(Looper.getMainLooper());
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pag);
         mPAGView = findViewById(R.id.pag_view);
-        initPagView();
+        mTvFrameRate = findViewById(R.id.tv_frame_rate);
+        initPagView2(mPAGView);
     }
 
-    private void initPagView() {
-        TimeDiffUtil.start();
+    private void initPagView(PAGImageView pagImageView) {
+        final int maxCount = 3;
+        int count = 0;
         PAGFile pagFile = PAGFile.Load(getAssets(), "charging_b.pag");
-        int layerType = pagFile.layerType();
-        int numImages = pagFile.numImages();
-        int numVideos = pagFile.numVideos();
-        int numChildren = pagFile.numChildren();
-        LogUtils.e(TAG, "afunx layerType: " + layerType);
-        LogUtils.e(TAG, "afunx numImages: " + numImages);
-        LogUtils.e(TAG, "afunx numVideos: " + numVideos);
-        LogUtils.e(TAG, "afunx numChildren: " + numChildren);
-        long consume = TimeDiffUtil.end();
-        LogUtils.i(TAG, "initPagView() load consume: " + consume + " ms");
-        TimeDiffUtil.start();
-        mPAGView.setComposition(pagFile);
-        consume = TimeDiffUtil.end();
-        LogUtils.i(TAG, "initPagView() setComposition consume: " + consume + " ms");
-        mPAGView.setRepeatCount(0);
-        mPAGView.play();
+
+
+        ++count;
+        PAGComposition pagComposition = PAGComposition.Make(512, 320);
+        if (count <= maxCount) {
+            pagComposition.addLayer(pagFile);
+        }
+
+        ++count;
+        Matrix matrix = new Matrix();
+        if (count <= maxCount) {
+            PAGFile pagFile1 = pagFile.copyOriginal();
+            matrix.postTranslate(0, -100);
+            pagFile1.setMatrix(matrix);
+            pagComposition.addLayer(pagFile1);
+        }
+
+        ++count;
+        if (count <= maxCount) {
+            PAGFile pagFile2 = pagFile.copyOriginal();
+            matrix.reset();
+            matrix.postTranslate(0, -50);
+            pagFile2.setMatrix(matrix);
+            pagComposition.addLayer(pagFile2);
+        }
+
+        ++count;
+        if (count <= maxCount) {
+            PAGFile pagFile3 = pagFile.copyOriginal();
+            matrix.reset();
+            matrix.postTranslate(0, 50);
+            pagFile3.setMatrix(matrix);
+            pagComposition.addLayer(pagFile3);
+        }
+
+        ++count;
+        if (count <= maxCount) {
+            PAGFile pagFile4 = pagFile.copyOriginal();
+            matrix.reset();
+            matrix.postTranslate(0, 100);
+            pagFile4.setMatrix(matrix);
+            pagComposition.addLayer(pagFile4);
+        }
+
+        pagImageView.setComposition(pagComposition);
+        pagImageView.setRepeatCount(0);
+        pagImageView.addListener(new PAGImageView.PAGImageViewListener() {
+
+            private final long interval = 1000;
+
+            private long lastTimestamp = 0;
+
+            private int frameCount = 0;
+
+            @Override
+            public void onAnimationStart(PAGImageView pagImageView) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(PAGImageView pagImageView) {
+
+            }
+
+            @Override
+            public void onAnimationCancel(PAGImageView pagImageView) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(PAGImageView pagImageView) {
+
+            }
+
+            @Override
+            public void onAnimationUpdate(PAGImageView pagImageView) {
+                long current = SystemClock.elapsedRealtime();
+                if (lastTimestamp == 0) {
+                    lastTimestamp = current;
+                } else {
+                    if (current - lastTimestamp >= interval) {
+                        ++frameCount;
+                        updateFrameRate(frameCount);
+                        frameCount = 0;
+                        lastTimestamp = current;
+                    } else {
+                        ++frameCount;
+                    }
+                }
+            }
+        });
+
+        pagImageView.play();
     }
 
+    private void initPagView2(PAGImageView pagImageView) {
+
+        PAGComposition pagComposition = PAGComposition.Make(512, 320);
+
+        PAGFile pagFileA = PAGFile.Load(getAssets(), "charging_a.pag");
+        pagComposition.addLayer(pagFileA);
+
+        PAGFile pagFileB = PAGFile.Load(getAssets(), "charging_b.pag");
+        pagComposition.addLayer(pagFileB);
+
+        PAGFile pagFileC = PAGFile.Load(getAssets(), "charging_c.pag");
+        pagComposition.addLayer(pagFileC);
+
+        pagImageView.setComposition(pagComposition);
+        pagImageView.setRepeatCount(0);
+        pagImageView.addListener(new PAGImageView.PAGImageViewListener() {
+
+            private final long interval = 1000;
+
+            private long lastTimestamp = 0;
+
+            private int frameCount = 0;
+
+            @Override
+            public void onAnimationStart(PAGImageView pagImageView) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(PAGImageView pagImageView) {
+
+            }
+
+            @Override
+            public void onAnimationCancel(PAGImageView pagImageView) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(PAGImageView pagImageView) {
+
+            }
+
+            @Override
+            public void onAnimationUpdate(PAGImageView pagImageView) {
+                long current = SystemClock.elapsedRealtime();
+                if (lastTimestamp == 0) {
+                    lastTimestamp = current;
+                } else {
+                    if (current - lastTimestamp >= interval) {
+                        ++frameCount;
+                        updateFrameRate(frameCount);
+                        frameCount = 0;
+                        lastTimestamp = current;
+                    } else {
+                        ++frameCount;
+                    }
+                }
+            }
+        });
+
+        pagImageView.play();
+    }
+
+    private void updateFrameRate(int rate) {
+        mMainHandler.removeCallbacksAndMessages(null);
+        mMainHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                mTvFrameRate.setText(String.valueOf(rate));
+            }
+        });
+    }
 
 }
