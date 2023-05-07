5be464b4334f6b65f8d2f83efe4cd42776801a9d
afunx <afunx.bai@ubtrobot.com>
2023-05-07 17:17:13 +0800

    代码调试中

--- app/src/main/java/me/afunx/xfun/app/PagActivity.java
+++ app/src/main/java/me/afunx/xfun/app/PagActivity.java
@@ -1,49 +1,44 @@
 package me.afunx.xfun.app;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-import androidx.databinding.ViewDataBinding;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.graphics.Matrix;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.SystemClock;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.afunx.xfun.common.utils.LogUtils;
 
 import org.libpag.PAGComposition;
-import org.libpag.PAGDiskCache;
 import org.libpag.PAGFile;
 import org.libpag.PAGImageView;
-import org.libpag.PAGPlayer;
-import org.libpag.PAGTimeStretchMode;
 import org.libpag.PAGView;
-
-import me.afunx.xfun.app.util.TimeDiffUtil;
 
 public class PagActivity extends AppCompatActivity {
 
     private static final String TAG = "PagActivity";
 
-    private PAGImageView mPAGView;
+    private PAGImageView mPAGImageView;
+
+    private PAGView mPAGView;
 
     private TextView mTvFrameRate;
 
-    private Handler mMainHandler = new Handler(Looper.getMainLooper());
+    private final Handler mMainHandler = new Handler(Looper.getMainLooper());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pag);
+        mPAGImageView = findViewById(R.id.pag_image_view);
         mPAGView = findViewById(R.id.pag_view);
         mTvFrameRate = findViewById(R.id.tv_frame_rate);
-        initPagView2(mPAGView);
+        initPagView2(mPAGImageView);
+        //initPagView3(mPAGView);
     }
 
     private void initPagView(PAGImageView pagImageView) {
@@ -207,6 +202,118 @@ public class PagActivity extends AppCompatActivity {
         });
 
         pagImageView.play();
+
+
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        int memClass = activityManager.getMemoryClass();//以M为单位，系统为应用分配的内存
+        int largeMemoryClass = activityManager.getLargeMemoryClass();//以M为单位，系统可提供给应用的最大内存使用值
+        LogUtils.e(TAG, "afunx memClass: " + memClass + ", largeMemoryClass: " + largeMemoryClass);
+        Float totalMemory = Runtime.getRuntime().totalMemory()*1.0f/(1024*1024);//以字节为单位转为M，该应用已占用内存（包含实际应用内存和空闲内存freeMemory）
+        Float freeMemory = Runtime.getRuntime().freeMemory()*1.0f/(1024*1024);//以字节为单位转为M，该应用已占用的空闲内存
+        Float maxMemory = Runtime.getRuntime().maxMemory()*1.0f/(1024*1024);//以字节为单位转为M，该应用最大可使用内存
+        LogUtils.e(TAG, "afunx totalMemory: " + totalMemory + ", freeMemory: " + freeMemory + ", maxMemory: " + maxMemory);
+    }
+
+    private void initPagView3(PAGView pagView) {
+
+        PAGComposition pagComposition = PAGComposition.Make(512, 320);
+
+        PAGFile pagFileA = PAGFile.Load(getAssets(), "charging_a.pag");
+        pagComposition.addLayer(pagFileA);
+
+        PAGFile pagFileB = PAGFile.Load(getAssets(), "charging_b.pag");
+        pagComposition.addLayer(pagFileB);
+
+        PAGFile pagFileC = PAGFile.Load(getAssets(), "charging_c.pag");
+        pagComposition.addLayer(pagFileC);
+
+        pagView.setComposition(pagComposition);
+        pagView.setRepeatCount(0);
+        pagView.addListener(new PAGView.PAGViewListener() {
+            @Override
+            public void onAnimationStart(PAGView pagView) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(PAGView pagView) {
+
+            }
+
+            @Override
+            public void onAnimationCancel(PAGView pagView) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(PAGView pagView) {
+
+            }
+
+            @Override
+            public void onAnimationUpdate(PAGView pagView) {
+
+            }
+        });
+        pagView.addListener(new PAGView.PAGViewListener() {
+
+            private final long interval = 1000;
+
+            private long lastTimestamp = 0;
+
+            private int frameCount = 0;
+
+            @Override
+            public void onAnimationStart(PAGView pagView) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(PAGView pagView) {
+
+            }
+
+            @Override
+            public void onAnimationCancel(PAGView pagView) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(PAGView pagView) {
+
+            }
+
+            @Override
+            public void onAnimationUpdate(PAGView pagView) {
+                long current = SystemClock.elapsedRealtime();
+                if (lastTimestamp == 0) {
+                    lastTimestamp = current;
+                } else {
+                    if (current - lastTimestamp >= interval) {
+                        ++frameCount;
+                        updateFrameRate(frameCount);
+                        frameCount = 0;
+                        lastTimestamp = current;
+                    } else {
+                        ++frameCount;
+                    }
+                }
+            }
+        });
+
+        pagView.setCacheEnabled(true);
+
+        pagView.play();
+
+
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        int memClass = activityManager.getMemoryClass();//以M为单位，系统为应用分配的内存
+        int largeMemoryClass = activityManager.getLargeMemoryClass();//以M为单位，系统可提供给应用的最大内存使用值
+        LogUtils.e(TAG, "afunx memClass: " + memClass + ", largeMemoryClass: " + largeMemoryClass);
+        Float totalMemory = Runtime.getRuntime().totalMemory()*1.0f/(1024*1024);//以字节为单位转为M，该应用已占用内存（包含实际应用内存和空闲内存freeMemory）
+        Float freeMemory = Runtime.getRuntime().freeMemory()*1.0f/(1024*1024);//以字节为单位转为M，该应用已占用的空闲内存
+        Float maxMemory = Runtime.getRuntime().maxMemory()*1.0f/(1024*1024);//以字节为单位转为M，该应用最大可使用内存
+        LogUtils.e(TAG, "afunx totalMemory: " + totalMemory + ", freeMemory: " + freeMemory + ", maxMemory: " + maxMemory);
     }
 
     private void updateFrameRate(int rate) {
@@ -215,6 +322,10 @@ public class PagActivity extends AppCompatActivity {
             @Override
             public void run() {
                 mTvFrameRate.setText(String.valueOf(rate));
+                Float totalMemory = Runtime.getRuntime().totalMemory()*1.0f/(1024*1024);//以字节为单位转为M，该应用已占用内存（包含实际应用内存和空闲内存freeMemory）
+                Float freeMemory = Runtime.getRuntime().freeMemory()*1.0f/(1024*1024);//以字节为单位转为M，该应用已占用的空闲内存
+                Float maxMemory = Runtime.getRuntime().maxMemory()*1.0f/(1024*1024);//以字节为单位转为M，该应用最大可使用内存
+                LogUtils.e(TAG, "afunx totalMemory: " + totalMemory + ", freeMemory: " + freeMemory + ", maxMemory: " + maxMemory);
             }
         });
     }
