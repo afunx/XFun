cb11fc7141cdff1201eb315e39e9a2c74decbd62
afunx <afunx.bai@ubtrobot.com>
2023-05-17 15:20:55 +0800

    修改BatteryRoundRect.java，解决绘制空隙问题

--- app/src/main/java/me/afunx/xfun/app/BatteryRoundRect.java
+++ app/src/main/java/me/afunx/xfun/app/BatteryRoundRect.java
@@ -2,6 +2,7 @@ package me.afunx.xfun.app;
 
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.RectF;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,9 @@ public class BatteryRoundRect {
     private final float mThickness;
     private final RectF mLeftRectF;
     private final RectF mRightRectF;
+
+    // 解决绘制空隙问题
+    private Path mLinePath = null;
 
     public BatteryRoundRect() {
         mThickness = BatteryMetrics.thickness();
@@ -46,5 +50,34 @@ public class BatteryRoundRect {
         canvas.drawRoundRect(mLeftRectF, mBigRadius, mBigRadius, paint);
         canvas.drawRoundRect(mRightRectF, mBigRadius, mBigRadius, paint);
         paint.setStyle(style);
+        if (mLinePath == null) {
+            mLinePath = getLinePath();
+        }
+        canvas.drawPath(mLinePath, paint);
+    }
+
+    private Path getLinePath() {
+        Path path = new Path();
+        final float thickness = BatteryMetrics.thickness();
+        final float bigWidth = BatteryMetrics.bigWidth();
+        final float bigHeight = BatteryMetrics.bigHeight();
+        final float left = BatteryMetrics.bigLeftMargin();
+        final float top = BatteryMetrics.bigTopMargin();
+        final float bottom = top + bigHeight;
+        final float cx = left + thickness;
+        final float cy = top + bigHeight / 2.0f;
+        final float length = (bottom - top) / 2.0f;
+        path.moveTo(cx, cy - length / 2.0f);
+        // 左圆左边
+        path.rLineTo(-2, 0);
+        path.rLineTo(4, 0);
+        path.rLineTo(0, length);
+        path.rLineTo(-4, 0);
+        path.close();
+        // 左圆右边
+        path.addPath(path, bigWidth - 2.0f * thickness, 0);
+        // 右圆左右两边
+        path.addPath(path, BatteryMetrics.bigBetweenMargin() + bigWidth, 0);
+        return path;
     }
 }
